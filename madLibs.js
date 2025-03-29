866c69696df997f722f27b7c644a4a03a262b29f
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2025-03-28 23:45:15 -0700

    [(KG]) Added and tested code for story.

---
+++ madLibs.js
@@ -0,0 +1,11 @@
+let story = `
+"On a stormy night, I ventured into the haunted house. The creaking walls seemed to whisper, sending chills down my spine. Suddenly, a ghost appeared, its glowing eyes piercing through the darkness. My heart raced as it floated silently toward me. Did you know 10 - 7 equals 3? Spooky, right?"
+`;
+  let timeOfDay = "stormy night";
+  let mathResult = 10 - 7;
+  let booleanValue = timeOfDay.includes("afternoon");
+  let person = {name: "Max", age: 28};
+
+document.getElementById("madlibs-output").textContent = story;
+
+console.log(story);
